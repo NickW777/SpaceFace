8d9af95fcf9113e215cef79879d976189b71204c
NEW777 <nwierzbow@gmail.com>
2024-04-12 10:40:02 -0400

    Got text from search bar

--- components/menu/AppHeaderActions.vue
+++ components/menu/AppHeaderActions.vue
@@ -1,35 +1,38 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-import MultiSelect from '../shared/MultiSelect.vue';
+import MultiSelect from '../shared/MultiSelect.vue'
 
 const options = [
   { value: 1, label: 'map-marker-outline' },
   { value: 2, label: 'star-outline' },
-  { value: 3, label: 'wheelchair-accessibility' },
+  { value: 3, label: 'wheelchair-accessibility' }
 ] as const
 
-type Value = typeof options[number]['value']
+type Value = (typeof options)[number]['value']
 
 const selectedFilter = ref<Value>(options[0].value)
 
 const searchActive = ref(false)
 const searchInput = ref<HTMLInputElement | null>(null)
 
-watch(searchActive, (isActive) => {
-  if (isActive) queueMicrotask(() => searchInput.value.focus())
-})
+// const search = ref<HTMLInputElement | null>(null)
 
+watch(searchActive, (isActive) => {
+  if (isActive)
+    queueMicrotask(() => {
+      // searchInput.focus()
+      console.log(searchInput.value)
+    })
+})
 </script>
 
 <template>
   <div class="flex justify-between h-8">
-
     <div
       :class="[
         'bg-white text-black rounded-full flex items-center transition-all duration-300 ease-in-out flex items-center',
-        searchActive ? 'w-full' : 'w-16',
+        searchActive ? 'w-full' : 'w-16'
       ]"
-
     >
       <button
         @click.stop="searchActive = !searchActive"
@@ -38,15 +41,12 @@ watch(searchActive, (isActive) => {
           'transition-all duration-300 ease-in-out cursor-pointer'
         ]"
       >
-        <mdicon
-          :name="searchActive ? 'chevron-left' : 'magnify'"
-          :size="searchActive ? 28 : 24"
-        />
+        <mdicon :name="searchActive ? 'chevron-left' : 'magnify'" :size="searchActive ? 28 : 24" />
       </button>
 
       <input
         v-show="searchActive"
-        ref="searchInput"
+        v-model="searchInput"
         type="text"
         class="w-2/3 outline-transparent"
         placeholder="Search"
@@ -58,7 +58,7 @@ watch(searchActive, (isActive) => {
       :options="options"
       :class="[
         !searchActive ? 'delay-100' : 'opacity-0',
-        'transition duration-200 ease-in-out w-[200px] absolute right-0',
+        'transition duration-200 ease-in-out w-[200px] absolute right-0'
       ]"
       type="icon"
     />
