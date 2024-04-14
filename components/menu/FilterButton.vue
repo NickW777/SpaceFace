0ee2ee83a32c58e82413e340fccb56cac72e2aaa
Will Saulnier <william.sau@kratosinnotech.com>
2024-04-14 19:44:24 -0400

    added button

---
+++ components/menu/FilterButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import LargeCircularButton from '../shared/LargeCircularButton.vue'
+
+const filterActive = ref(false);
+
+watch(filterActive, (isActive) => {
+  if (isActive) alert("active")
+})
+
+</script>
+
+<template>
+  <div>
+  
+  </div>
+  <LargeCircularButton
+  @click.stop="filterActive = !filterActive"
+  :icon="filterActive ? 'window-close' : 'tune'"
+  class="bg-study-space-light text-white absolute bottom-5 right-5"
+  />
+</template>
