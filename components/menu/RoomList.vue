b0390a4b842b174d0954d5122c0a348bbbff0650
NEW777 <nwierzbow@gmail.com>
2024-04-30 18:01:15 -0400

    main page loads properly

--- components/menu/RoomList.vue
+++ components/menu/RoomList.vue
@@ -18,8 +18,12 @@ const rooms = computed(() => roomStore?.getPage(0)?.rooms)
 
 // yonas note: this was inlined inside the template, this breaks the declarative nature of Vue. It's better to have this in the script setup
 const toggleDetail = async () => {
+  roomStore.setDetailRoom(0, 1)
   //Open the room detail view
   roomStore.toggleDetail()
+  fetchCompleteSpaceProvider(roomStore.getPage(0).rooms[1]._id).then((data) => {
+    roomStore.storeCompleteRoom(0, 1, data)
+  })
   //Don't query BlockMap if that room has already been queried
   if (roomStore.getRoomAvailability('BART_0065') === undefined) {
     roomStore.startLoadingRoomAvailability()
@@ -31,47 +35,19 @@ const toggleDetail = async () => {
 
 <template>
   <div class="flex flex-wrap overflow-auto py-3 px-2 relative w-full">
-
-    <div
-      v-for="room in rooms"
-      :key="room._id"
-      class="w-1/2 px-1 pb-2"
-    >
-
+    <div v-for="room in rooms" :key="room._id" class="w-1/2 px-1 pb-2">
       <RoomCard
-        @click.stop="
-          () => {
-            //Choose the room and open the room detail view
-            roomStore.setDetailRoom(0, i - 1)
-            roomStore.toggleDetail()
-            fetchCompleteSpaceProvider(roomStore.getPage(0).rooms[i - 1]._id).then((data) => {
-              roomStore.storeCompleteRoom(0, i - 1, data)
-            })
-
-            //Don't query Blockmap if that room has already been queried
-            if (roomStore.getRoomAvailability('BART_0065') === undefined) {
-              roomStore.startLoadingRoomAvailability()
-              fetchBlockMap('BART_0065').then((data) => roomStore.storeRoomAvailability(data))
-            }
-          }
-        "
-        :building="roomStore.getPage(0).rooms[i - 1].building"
-        :room="roomStore.getPage(0).rooms[i - 1].room"
-        :thumbnail="roomStore.getPage(0).rooms[i - 1].images?.[0] || '/images/imageNotFound.jpg'"
+        @click.stop="toggleDetail"
+        :building="room.building"
+        :room="room.room"
+        :thumbnail="room.images?.[0] || '/images/imageNotFound.jpg'"
         availability="Available For Another 3 Hours (Until 5pm)"
+        :labels="room.labels"
       />
-
     </div>
 
-    <div
-      v-if="loading"
-      v-for="i in 50"
-      :key="i"
-      class="w-1/2 px-1 pb-2"
-    >
+    <div v-if="loading" v-for="i in 50" :key="i" class="w-1/2 px-1 pb-2">
       <RoomCardSkeleton />
     </div>
-
   </div>
-
 </template>
