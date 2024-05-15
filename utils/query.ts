37f4361e4ce15f0d0304eb2dae8938e4c531e92b
Will Saulnier <wsaulnier1004@gmail.com>
2024-05-14 21:32:54 -0400

    filter potentially works. dependant on spaceprovider

--- utils/query.ts
+++ utils/query.ts
@@ -12,17 +12,23 @@ import { useFetch } from '@vueuse/core'
 //Query SpaceProvider
 
 export async function fetchSpaceProvider(q: string, filterArr:string[], page: number): Promise<SpaceProviderType> {
-  const position = await new Promise<GeolocationPosition>((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject)
-  })
 
-  const lat = position.coords.latitude
-  const lon = position.coords.longitude
+  // Note from Will: Latitude and longitude isnt working smoothly for me, so its commented out
+  // const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+  //   navigator.geolocation.getCurrentPosition(resolve, reject)
+  // })
+  // const lat = position.coords.latitude
+  // const lon = position.coords.longitude
+  // console.log(`Fetching SpaceProvider with query: ${q}`, `lat: ${lat}`, `lon: ${lon}, `, filterArr)
 
-  console.log(`Fetching SpaceProvider with query: ${q}`, `lat: ${lat}`, `lon: ${lon}, `, filterArr)
+  console.log("fetching SpaceProvider")
+
+  // Parse filterArr into queryable string
+  let filterQuery = "";
+  filterArr.forEach(label => filterQuery += " label:" + label)
 
   const { data } = await useFetch<string>(
-    `https://spaceprovider.up.railway.app/api/v1?q=${q}&page=${page}&limit=10&lat=${lat}&lon=${lon}`
+    `https://spaceprovider.up.railway.app/api/v1?q=${q}${filterQuery}&page=${page}&limit=10`
   ).get()
 
   return SpaceProvider.parse(JSON.parse(data.value ?? ''))
