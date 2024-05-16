c808cbe729b4a7b7a14b1c38db07435888bca062
NEW777 <nwierzbow@gmail.com>
2024-05-16 17:26:01 -0400

    Added location querying back in

--- utils/query.ts
+++ utils/query.ts
@@ -11,24 +11,27 @@ import { useFetch } from '@vueuse/core'
 
 const SPACE_PROVIDER_URI = 'https://spaceprovider.up.railway.app/api/v1'
 
-export async function fetchSpaceProvider(q: string, filterArr:string[], page: number): Promise<SpaceProviderType> {
-  console.log("fetching SpaceProvider")
+export async function fetchSpaceProvider(
+  q: string,
+  filterArr: string[],
+  page: number
+): Promise<SpaceProviderType> {
+  console.log('fetching SpaceProvider')
   // Note from Will: Latitude and longitude isnt working smoothly for me, so its commented out
-  // const position = await new Promise<GeolocationPosition>((resolve, reject) => {
-  //   navigator.geolocation.getCurrentPosition(resolve, reject)
-  // })
-  // const lat = position.coords.latitude
-  // const lon = position.coords.longitude
+  const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+  const lat = position.coords.latitude
+  const lon = position.coords.longitude
   // console.log(`Fetching SpaceProvider with query: ${q}`, `lat: ${lat}`, `lon: ${lon}, `, filterArr)
 
-
   // Parse filterArr into queryable string
-  let filterQuery = "";
-  filterArr.forEach(label => filterQuery += " label:" + label)
+  let filterQuery = ''
+  filterArr.forEach((label) => (filterQuery += ' label:' + label))
   // console.log(`${SPACE_PROVIDER_URI}?q=${q}${filterQuery}&page=${page}&limit=10`)
 
   const { data } = await useFetch<string>(
-    `${SPACE_PROVIDER_URI}?q=${q}${filterQuery}&page=${page}&limit=10`
+    `${SPACE_PROVIDER_URI}?q=${q}${filterQuery}&page=${page}&limit=10&lat=${lat}&lon=${lon}`
   ).get()
 
   return SpaceProvider.parse(JSON.parse(data.value ?? ''))
