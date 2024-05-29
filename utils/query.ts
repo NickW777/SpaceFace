aa7e61b7df97dc1de0fce89b618d848d5ea00134
Will Saulnier <wsaulnier1004@gmail.com>
2024-05-29 15:21:44 -0400

    remove location querying due to browser issues

--- utils/query.ts
+++ utils/query.ts
@@ -18,11 +18,11 @@ export async function fetchSpaceProvider(
 ): Promise<SpaceProviderType> {
   console.log('fetching SpaceProvider')
   // Note from Will: Latitude and longitude isnt working smoothly for me, so its commented out
-  const position = await new Promise<GeolocationPosition>((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject)
-  })
-  const lat = position.coords.latitude
-  const lon = position.coords.longitude
+  // const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+  //   navigator.geolocation.getCurrentPosition(resolve, reject)
+  // })
+  // const lat = position.coords.latitude
+  // const lon = position.coords.longitude
   // console.log(`Fetching SpaceProvider with query: ${q}`, `lat: ${lat}`, `lon: ${lon}, `, filterArr)
 
   // Parse filterArr into queryable string
@@ -31,7 +31,7 @@ export async function fetchSpaceProvider(
   // console.log(`${SPACE_PROVIDER_URI}?q=${q}${filterQuery}&page=${page}&limit=10`)
 
   const { data } = await useFetch<string>(
-    `${SPACE_PROVIDER_URI}?q=${q}${filterQuery}&page=${page}&limit=10&lat=${lat}&lon=${lon}`
+    `${SPACE_PROVIDER_URI}?q=${q}${filterQuery}&page=${page}&limit=10`
   ).get()
 
   return SpaceProvider.parse(JSON.parse(data.value ?? ''))
